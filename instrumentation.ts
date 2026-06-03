52cd75f8139a8b9aa8c1f2829bba0425551d6654
Austen DeWolf <8270779+austendewolf@users.noreply.github.com>
2026-06-02 23:14:32 -0700

    Revert "Merge pull request #1 from austendewolf/praetom/install-runtime-mpxntyvo"
    This reverts commit 91ecf2217dc1d45ef36e69a0aa21e435a713c5ee, reversing
    changes made to 739fd4b59a81aca024855ae388819c3b21067745.

--- instrumentation.ts
+++
@@ -1,6 +0,0 @@
-import { register as praetomRegister } from "praetom";
-
-export async function register() {
-  if (process.env.NEXT_RUNTIME !== "nodejs") return;
-  await praetomRegister({ ingestToken: "praetom_pub_qd-RXZArvEdG4ymqTzsVIli8bfTB7Btn" });
-}
